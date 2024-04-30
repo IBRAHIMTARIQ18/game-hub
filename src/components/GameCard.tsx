2828e943b25daf23f028038b53dcc6d9a58b28b4
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2024-04-30 07:22:33 +0500

    Displaying plaform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -11,6 +12,7 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)} />
       </CardBody>
     </Card>
   );
