d3519de9d20908332fe9842d28bc8f6c8be4f00b
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2024-04-30 08:28:37 +0500

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardBody,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width='400px' borderRadius={10} overflow="hidden">
+      <Skeleton height='275px' />
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
