1ac0db3db5c17d342bd0f3fb328b256860448a89
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2026-05-04 23:41:23 +0500

    GameModal component created

---
+++ src/components/GameModal.tsx
@@ -0,0 +1,48 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  game: any;
+}
+
+const GameModal = ({ isOpen, onClose, game }: Props) => {
+  if (!game) return null;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size={"2xl"}>
+      <ModalOverlay borderRadius={60} />
+      <ModalContent
+        bg="gray.800"
+        color="white"
+        fontSize={"large"}
+        borderRadius={25}
+      >
+        <ModalHeader fontSize={"x-large"} textAlign={"center"}>
+          {game.name}
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Image src={game.background_image} borderRadius="10px" />
+          <Text mt={3} textAlign={"center"}>
+            ⭐ Rating: {game.rating}
+          </Text>
+          <Text mt={3} mb={3} textAlign={"center"}>
+            📅 Released: {game.released}
+          </Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default GameModal;
